0b08e0c894644efc1335b8b3abad9c1fe270a59d
Cronabit 1 <cronabit1@Cronabits-Mac-mini.local>
2017-11-09 15:55:17 +0530

    Initial Commit

---
+++ 007/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  007
+//
+//  Created by Cronabit 1 on 09/11/17.
+//  Copyright © 2017 Cronabit 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
